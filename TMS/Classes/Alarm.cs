fb7ea610aac1a356181644eaf7438d10b49c5b64
Atle-André Gundersen <atleag@outlook.com>
2016-03-11 09:30:03 +0100

    Updated Alarm Class

--- TMS/Classes/Alarm.cs
+++ TMS/Classes/Alarm.cs
@@ -6,7 +6,23 @@ using System.Threading.Tasks;
 
 namespace TMS
 {
+     
     class Alarm
     {
+        int _myProperty = 0;
+
+
+        public int MyProperty
+        {
+            get { return _myProperty; }
+            set
+            {
+                _myProperty = value;
+                if (_myProperty == 1)
+                {
+                    // DO SOMETHING HERE
+                }
+            }
+        }
     }
 }
